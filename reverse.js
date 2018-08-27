54e20e40c085d131fad8c1171e6da6d5a4f19538
Daniyar Salikhov <salihovdr@gmail.com>
2018-08-27 12:16:18 -0400

    added alternative solution to reverse drill

--- reverse.js
+++ reverse.js
@@ -1,9 +1,16 @@
 // input: 'hello';
 // output: 'olleh';
 
-function reverse(str){
-  if(!str) return '';
-  return reverse(str.slice(1)) + str[0];
+// solution I
+// function reverse(str){
+//   if(!str) return '';
+//   return reverse(str.slice(1)) + str[0];
+// }
+
+// solution II
+function reverse(str) {
+  if (!str) return '';
+  return str[str.length-1] + reverse(str.slice(0, str.length-1));
 }
 
 console.log(reverse('hello'));
