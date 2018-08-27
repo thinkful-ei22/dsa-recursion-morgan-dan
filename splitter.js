7f6a691138004db3f5365f3c4ff09dbeb0b80f43
Daniyar Salikhov <salihovdr@gmail.com>
2018-08-27 15:04:39 -0400

    split drill half-complete

---
+++ splitter.js
@@ -0,0 +1,43 @@
+// input: apple, '';
+// output: ['a', 'p', 'p', 'l', 'e'];
+
+function split(str, splitter, currentString = '') {
+  if (!str && currentString.length === 0) return [];
+  if (str.length === 0 && currentString) {
+    return [currentString];
+  }
+  if (str[0] === splitter) {
+    return [currentString, ...split(str.slice(1), splitter, '')];
+  } else {
+    currentString += str[0];
+    return [...split(str.slice(1), splitter, currentString)];
+  }
+}
+
+// function split(str, splitter, currentString = ''){
+
+//   let arr=[];
+//   if(!str && splitter.length===0) {
+//     return [];
+//   }
+//   if(!str){
+//     return [currentString]; 
+//   }
+
+//   if (splitter.length===0) {
+//     if(str[0]!==splitter) currentString+=str[0];
+//     else {
+//       arr.push(currentString);
+//     }
+//     return [currentString, ...split(str.slice(1), splitter)];
+//   } else {
+//     if (str[0] !== splitter) currentString += str[0];
+//     else {
+//       arr.push(currentString);
+//       return [currentString, ...split(str.slice(1), splitter, '')]
+//     }
+//     return [...split(str.slice(1), splitter, currentString)];
+//   }
+
+// }
+console.log(split('apple', 'le'));
