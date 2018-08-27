55483b995981d31c4ab2dbad046b281d7afc3b5e
Daniyar Salikhov <salihovdr@gmail.com>
2018-08-27 11:50:27 -0400

    jump exercise completed

---
+++ jump.js
@@ -0,0 +1,13 @@
+// Input: 3
+// Output:
+// 3 - Another sheep jump over the fence
+// 2 - Another sheep jump over the fence
+// 1 - Another sheep jump over the fence
+
+function jump(n){
+  if(n===0) return;
+  console.log(`${n} - Another sheep jump over the fence`);
+  jump(n-1);
+}
+
+jump(3);
